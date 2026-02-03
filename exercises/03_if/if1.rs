fdf00cfd73f736f8265ea6990be78a7eefd499a3
Vu Chuc <vuchuc781999@gmail.com>
2026-02-03 12:07:48 +0000

    03/02/2026

--- exercises/03_if/if1.rs
+++ exercises/03_if/if1.rs
@@ -4,6 +4,14 @@ fn bigger(a: i32, b: i32) -> i32 {
     // Do not use:
     // - another function call
     // - additional variables
+
+    // if a > b {
+    //     return a;
+    // }
+    //
+    // b
+
+    if a > b { a } else { b }
 }
 
 fn main() {
